3c8a97413a49da2fb489a92cb7f082e08b57825a
Anjali-st <anjali90908080@gmail.com>
2023-07-28 15:40:18 +0530

    Assignment-18

---
+++ IRechargeable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Assignment_18
+{
+    public interface IRechargeable
+    {
+        int Charge(int minutes);
+    }
+}
+
